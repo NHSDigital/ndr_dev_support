18e533e0b99bfb8aebe4ccbcff4bc2102619ab33
Josh Pencheon <josh.pencheon@googlemail.com>
2016-02-05 14:17:15 +0000

    # version 1.1.2

--- lib/ndr_dev_support/version.rb
+++ lib/ndr_dev_support/version.rb
@@ -1,5 +1,5 @@
 # This defines the NdrDevSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks (and our own).
 module NdrDevSupport
-  VERSION = '1.1.1'.freeze
+  VERSION = '1.1.2'.freeze
 end
