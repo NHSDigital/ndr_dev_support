14716806f63058b1d5f3485e2380302fa6ef7526
Josh Pencheon <josh.pencheon@googlemail.com>
2016-07-11 17:21:34 +0100

    # Allow multiple shared connections

--- lib/ndr_dev_support/integration_testing/connection_sharing.rb
+++ lib/ndr_dev_support/integration_testing/connection_sharing.rb
@@ -6,19 +6,19 @@ module NdrDevSupport
     #
     # Derived from: https://gist.github.com/josevalim/470808
     #
+    # Modified to support multiple connection pools
+    #
     module ConnectionSharing
       def self.prepended(base)
-        base.mattr_accessor :shared_connection
-
-        # Get a connection, and make it shareable:
-        base.shared_connection = base.connection
+        base.mattr_accessor :shared_connections
+        base.shared_connections = {}
 
         base.singleton_class.prepend(ClassMethods)
       end
 
       module ClassMethods
         def connection
-          shared_connection || retrieve_connection
+          shared_connections[connection_config] ||= retrieve_connection
         end
       end
     end
