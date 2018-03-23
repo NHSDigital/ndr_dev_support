8e076cbdbac3a1d65e955457b58e42f4ed6ff152
timgentry <tpgentry@me.com>
2018-03-23 10:36:11 +0000

    # Bump to v2.2.0

--- lib/ndr_dev_support/version.rb
+++ lib/ndr_dev_support/version.rb
@@ -2,5 +2,5 @@
 # This defines the NdrDevSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks (and our own).
 module NdrDevSupport
-  VERSION = '2.1.2'.freeze
+  VERSION = '2.2.0'.freeze
 end
