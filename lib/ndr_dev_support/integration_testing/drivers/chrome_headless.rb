e13e0f3429ce231c567dc6573e64897aa4bb4783
Josh Pencheon <josh@Joshs-2016-MBR.local>
2018-04-13 15:56:06 +0100

    # boyscout

--- lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
+++ lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
@@ -2,12 +2,14 @@ require 'selenium-webdriver'
 
 Capybara.register_driver(:chrome_headless) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'chromeOptions' => { args: %w(headless disable-gpu) }
+    'chromeOptions' => { args: %w[headless disable-gpu] }
   )
 
-  Capybara::Selenium::Driver.new app,
+  Capybara::Selenium::Driver.new(
+    app,
     browser: :chrome,
     desired_capabilities: capabilities
+  )
 end
 
 Capybara::Screenshot.register_driver(:chrome_headless) do |driver, path|
