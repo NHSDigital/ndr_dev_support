fcf0654e9e43222664cf7821c5d43f5584824f7c
Josh Pencheon <josh.pencheon@googlemail.com>
2019-05-30 11:09:06 +0100

    # add chrome headless switches to fix timeout issues

--- lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
+++ lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
@@ -2,7 +2,13 @@ require 'selenium-webdriver'
 
 Capybara.register_driver(:chrome_headless) do |app|
   capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    'chromeOptions' => { args: %w[headless disable-gpu] }
+    chromeOptions: {
+      args: %w[
+        headless disable-gpu no-sandbox
+        --window-size=1920,1080
+        --enable-features=NetworkService,NetworkServiceInProcess
+      ]
+    }
   )
 
   Capybara::Selenium::Driver.new(
