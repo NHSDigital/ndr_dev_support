0409b687d31202b2a4281ebfb7b0c330cc78d904
Josh Pencheon <josh.pencheon@googlemail.com>
2019-06-18 14:06:17 +0100

    # update chrome_headless definition following Chrome 75 changes

--- lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
+++ lib/ndr_dev_support/integration_testing/drivers/chrome_headless.rb
@@ -1,22 +1,18 @@
 require 'selenium-webdriver'
 require 'show_me_the_cookies'
 
-Capybara.register_driver(:chrome_headless) do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[
-        headless disable-gpu no-sandbox
-        --window-size=1920,1080
-        --enable-features=NetworkService,NetworkServiceInProcess
-      ]
-    }
-  )
-
-  Capybara::Selenium::Driver.new(
-    app,
-    browser: :chrome,
-    desired_capabilities: capabilities
-  )
+Capybara.register_driver :chrome_headless do |app|
+  Capybara::Selenium::Driver.load_selenium
+  browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
+    opts.args << '--headless'
+    opts.args << '--disable-gpu' if Gem.win_platform?
+    opts.args << '--no-sandbox'
+    # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
+    opts.args << '--disable-site-isolation-trials'
+    opts.args << '--window-size=1920,1080'
+    opts.args << '--enable-features=NetworkService,NetworkServiceInProcess'
+  end
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: browser_options)
 end
 
 Capybara::Screenshot.register_driver(:chrome_headless) do |driver, path|
