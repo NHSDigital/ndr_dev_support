aaa9a5512e8600b3a3bca5c9fdffaf615e3ba64d
Josh Pencheon <josh.pencheon@googlemail.com>
2019-07-24 15:11:08 +0100

    # avoid triggering a minitest autodiscover in all spawned shells
    Would lead to Rails being partially loaded in spawned Ruby process, breaking e.g. webpacker asset compilation

--- lib/minitest/rake_ci.rb
+++ lib/minitest/rake_ci.rb
@@ -1,6 +1,9 @@
 require 'minitest'
 
-# Autodiscover the _plugin.rb file:
-Minitest.load_plugins
-Minitest::RakeCIReporter.enable!
+# Don't trigger full plugin autodiscovery, as we load this
+# file through RUBYOPT extension (and full autodiscovery can
+# lead to e.g. incomplete definitions of Rails existing).
+require_relative 'rake_ci_plugin'
+Minitest.extensions << 'rake_ci'
 
+Minitest::RakeCIReporter.enable!
