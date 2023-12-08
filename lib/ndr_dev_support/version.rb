251a8b5dcf3367b6ef5ce2e0a2b502651a5a994b
Kenny Lee <kennylee@nhs.net>
2023-12-08 13:31:31 +0000

    7.2.2

--- lib/ndr_dev_support/version.rb
+++ lib/ndr_dev_support/version.rb
@@ -2,5 +2,5 @@
 # This defines the NdrDevSupport version. If you change it, rebuild and commit the gem.
 # Use "rake build" to build the gem, see rake -T for all bundler rake tasks (and our own).
 module NdrDevSupport
-  VERSION = '7.2.1'
+  VERSION = '7.2.2'
 end
