227a40c2e3fb8104a94178b56a7f75d7a28fe058
Brian Shand <Brian.Shand@nhs.net>
2024-12-10 16:56:17 +0000

    Install rbenv, ruby-build and updated ruby versions from vendor/rbenv/
    Better rbenv installation error handling

--- lib/ndr_dev_support/capistrano/install_ruby.rb
+++ lib/ndr_dev_support/capistrano/install_ruby.rb
@@ -3,8 +3,10 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc <<~DESC
       Ensure that the required ruby version is installed.
 
-      An offline copy of rbenv should be placed in /opt/rbenv.tar.gz
-      To make this file, for ruby 3.1.6, run the following commands:
+      This can be installed from /opt/rbenv.tar.gz (first installation only) or vendor/rbenv/
+
+      To place an offline copy of rbenv in /opt/rbenv.tar.gz
+      For ruby 3.1.6, run the following commands:
       $ mkdir clone_rbenv
       $ git clone https://github.com/rbenv/rbenv.git clone_rbenv/.rbenv
       $ git clone https://github.com/rbenv/ruby-build.git clone_rbenv/.rbenv/plugins/ruby-build
@@ -13,6 +15,21 @@ Capistrano::Configuration.instance(:must_exist).load do
       $ (cd clone_rbenv; rm -f ../rbenv.tar.gz; tar czf ../rbenv.tar.gz .rbenv)
       $ rm -rf clone_rbenv
       $ scp -p rbenv.tar.gz app-server:/opt/rbenv.tar.gz
+
+      To add rbenv, ruby-build and additional ruby versions to the application vendor directory
+      For ruby 3.2.6:
+      $ mkdir clone_rbenv
+      $ git clone https://github.com/rbenv/rbenv.git clone_rbenv/.rbenv
+      $ mkdir -p vendor/rbenv; rm -f vendor/rbenv/rbenv.tar.gz
+      $ tar czf vendor/rbenv/rbenv.tar.gz -C clone_rbenv .rbenv
+      $ rm -rf clone_rbenv
+      $ mkdir clone_ruby-build
+      $ git clone https://github.com/rbenv/ruby-build.git clone_ruby-build/ruby-build
+      $ mkdir -p vendor/rbenv/cache; rm -f vendor/rbenv/ruby-build.tar.gz
+      $ tar czf vendor/rbenv/ruby-build.tar.gz -C clone_ruby-build ruby-build
+      $ rm -rf clone_ruby-build
+      $ (cd vendor/rbenv/cache; curl -O https://cache.ruby-lang.org/pub/ruby/3.2/ruby-3.2.6.tar.gz)
+      $ git add vendor/rbenv/{rbenv,ruby-build}.tar.gz vendor/rbenv/cache/*
     DESC
     task :install_ruby do
       version = fetch(:ruby)
@@ -24,27 +41,47 @@ Capistrano::Configuration.instance(:must_exist).load do
       # For some reason, SSH keepalive options have no effect with capistrano 2 and net-ssh 7.
       # So we run a poor-man's keepalive, because this installation can take over 10 minutes
       # and some of our SSH servers disconnect inactive sessions after 5 minutes.
+      # We have a keepalive time limit of 40 minutes in case the installation fails unexpectedly.
       #
       # We remove ~/.rbenv paths from capistrano-defined PATH when running `rbenv init`
       # so that it knows the path is needed in ~/.bash_profile
+      #
+      # We use latest_release: this is broadly well-defined, and will point to the in-progress
+      # release if we're part way through a deployment, or the most recent release if run
+      # after a deployment has happened, or be blank if attempted after cap deploy:setup
       run <<~SHELL
         set -e;
-        if ! rbenv versions --bare 2> /dev/null | grep -qF #{version}; then
+        if ! rbenv versions --bare 2> /dev/null | grep -q ^#{Regexp.escape(version)}$; then
           echo Installing ruby #{version};
-          { while true; do sleep 20; echo -n '.'; done & } 2> /dev/null;
+          { sleep 10; for i in `seq 1 80`; do echo -n '.'; sleep 30; done & } 2> /dev/null;
           sudo -i -n -u #{fetch(:application_user)} sh -c "
             if [ ! -e .rbenv ] && [ -e /opt/rbenv.tar.gz ]; then
               tar xf /opt/rbenv.tar.gz .rbenv;
               PATH=\\`echo \\"\\$PATH\\"|sed -e \\"s_[^:=]*/[.]rbenv[^:]*:__g\\"\\` .rbenv/bin/rbenv init bash;
             fi;
-            eval \\"\\$(.rbenv/bin/rbenv init - --no-rehash bash)\\";
-            export TMPDIR=\\`mktemp -d \\"\\$HOME\\"/rbenv_tmp_XXXX\\`;
-            set +e;
-            rbenv install #{version} --skip-existing 2>&1;
-            set -e;
-            RBENV_VERSION=#{version} ruby --version;
-            rm -rf \\"\\`printenv TMPDIR\\`\\";
-            rbenv global #{version};
+            if [ ! -e .rbenv ] && [ -f #{latest_release}/vendor/rbenv/rbenv.tar.gz ]; then
+              tar xf #{latest_release}/vendor/rbenv/rbenv.tar.gz .rbenv;
+              PATH=\\`echo \\"\\$PATH\\"|sed -e \\"s_[^:=]*/[.]rbenv[^:]*:__g\\"\\` .rbenv/bin/rbenv init bash;
+            fi;
+            if [ ! -e .rbenv ]; then
+              echo rbenv not installed: aborting;
+            else
+              if [ -d #{latest_release}/vendor/rbenv/cache ] && [ -n \\"\\`ls #{latest_release}/vendor/rbenv/cache\\`\\" ]; then
+                mkdir -p .rbenv/cache/;
+                cp -nvp #{latest_release}/vendor/rbenv/cache/* .rbenv/cache/;
+              fi;
+              if [ -f #{latest_release}/vendor/rbenv/ruby-build.tar.gz ]; then
+                mkdir -p .rbenv/plugins/;
+                tar xf #{latest_release}/vendor/rbenv/ruby-build.tar.gz -C .rbenv/plugins/ ruby-build;
+              fi;
+              eval \\"\\$(.rbenv/bin/rbenv init - --no-rehash bash)\\";
+              export TMPDIR=\\`mktemp -d \\"\\$HOME\\"/rbenv_tmp_XXXX\\`;
+              if rbenv install #{version} --skip-existing 2>&1; then
+                RBENV_VERSION=#{version} ruby --version;
+                rm -rf \\"\\`printenv TMPDIR\\`\\";
+                rbenv global #{version};
+              fi;
+            fi;
           ";
           { kill % && wait; } 2> /dev/null;
           set -e;
